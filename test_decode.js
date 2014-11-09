0610cd266c51693361fd3e382e8a9eabe9468d47
Dean McNamee <dean@gmail.com>
2014-11-09 10:28:57 +0100

    Use the structs code even for token packets.

--- test_decode.js
+++ test_decode.js
@@ -31,6 +31,8 @@ function process_packet(packet) {
 
   var text = "";
 
+  var fields = [ ];
+
   switch (group) {
     case 0:  // Special
       text += "special";
@@ -40,14 +42,10 @@ function process_packet(packet) {
       if (plen != 3) throw "token packet length != 3";
       var type = ["OUT", "SOF", "IN", "SETUP"][pid >> 2];
       text += "token " + type;
-      if (pid >> 2 === 1) {  // SOF
-        var framenum = packet[1] | (packet[2] << 8 & 0x700);
-        text += " frame: " + framenum;
-      } else {
-        var addr = packet[1] & 0x7f;
-        var endp = (packet[1] >> 7 & 1) | (packet[2] << 1 & 0xe);
-        text += " addr: 0x" + addr.toString(16) + " endp: " + endp;
-      }
+
+      var parser = (pid >> 2 === 1) ? structs.parse_StartOfFramePacket : structs.parse_TokenPacket;
+      parser(fields, packet, 1, packet.length);
+
       var crc = crclib.crc5_16bit(packet[1], packet[2]);
       if (crc !== 6) text += " BADCRC5: 0x" + crc.toString(16);
       if (pid >> 2 === 3) next_is_setup = true;
@@ -62,18 +60,18 @@ function process_packet(packet) {
       var crc = crclib.crc16(packet, 1);
       if (crc !== 0xb001) text += " BADCRC16: 0x" + crc.toString(16);
       if (next_is_setup === true) {
-        var fields = [ ];
         structs.parse_setup(fields, packet, 1, packet.length-2);
-        for (var i = 0, il = fields.length; i < il; i += 3) {
-          text += ' ' + fields[i] + ': 0x' + fields[i+1].toString(16);
-          if (fields[i+2] !== null) text += ' (' + fields[i+2] + ')';
-        }
         next_is_setup = false;
       }
       break;
   }
 
   if (text.length !== 0) console.log(text);
+  for (var i = 0, il = fields.length; i < il; i += 3) {
+    var ftext = '  ' + fields[i] + ': 0x' + fields[i+1].toString(16);
+    if (fields[i+2] !== null) ftext += ' (' + fields[i+2] + ')';
+    console.log(ftext);
+  }
 }
 
 function forAsyncEachLine(stream, line_callback, eof_callback) {
