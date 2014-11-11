8caeecc1e8d9f989f63e900726bf8f62b0ec473e
Dean McNamee <dean@gmail.com>
2014-11-11 13:03:16 +0100

    Test html dumping of huge <table>.  Bad performance.

---
+++ test_html.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var structs = require('./structs.js');
+var transfer_machine = require('./transfer_machine.js');
+
+if (process.argv.length > 2) {
+  eval(fs.readFileSync(process.argv[2], 'utf8'));
+} else {
+  console.log("usage: <filename.js>");
+  process.exit(1);
+}
+
+function octets_to_hex_string(octets) {
+  return octets.map(function(x) {
+    var str = x.toString(16);
+    return str.length === 1 ? "0" + str : str;
+  }).join(' ');
+}
+
+function flatten(arr) {
+  return arr.length === 0 ? arr : arr.reduce(function(a, b) { return a.concat(b); });
+}
+
+var machine = new transfer_machine.TransferMachine();
+
+machine.OnControlTransfer = function(addr, endp, setup, data) {
+  console.log("Control transfer: addr: " + addr + " endpoint: " + endp);
+  var bRequest = setup.get_value("bRequest");
+  console.log(setup.debug_string("  "));
+  data = flatten(data);
+  if (data.length > 0) console.log('    Data: ' + octets_to_hex_string(data));
+
+  switch (bRequest) {
+    case 6: // GET_DESCRIPTOR
+      var wvalue = setup.get_value("wValue");
+      var desctype = wvalue >> 8, descidx = wvalue & 0xff;
+      console.log(structs.eDescriptorTypes[desctype]);
+      var descriptor = new structs.Fields();
+      console.log(data.length);
+      if (structs.parse_StandardConfigurationDescriptor(descriptor, data, 0, data.length))
+        console.log(descriptor.debug_string("    "));
+      break;
+    default:
+      console.log("Unknown bRequest: " + bRequest);
+      break;
+  }
+};
+
+var decoder = require('./packet_decoder.js');
+console.log('<html><head><link rel="stylesheet" href="usbbarev0.css" /></head><body><table class="usbbare-pd">');
+for (var i = 0, il = packets.length; i < il; ++i) {
+  var packet = packets[i];
+  var desc = decoder.decode_packet_to_display_string(packet.d);
+  console.log('<tr><td>' + i + '</td><td>' + packet.t + '</td><td>' +
+               packet.f + '</td><td>' + desc + '<td></tr>');
+  //if (packet.d.length !== 0) machine.process_packet(packet.d);
+}
+console.log('</table></body></html>');
