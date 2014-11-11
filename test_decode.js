9a4c7caf4c15dc8d844f91f2e8f706c97555b5aa
Dean McNamee <dean@gmail.com>
2014-11-11 19:11:41 +0100

    Make test_decode work on js files not custom format.

--- test_decode.js
+++ test_decode.js
@@ -1,32 +1,12 @@
+var fs = require('fs');
 var structs = require('./structs.js');
 var transfer_machine = require('./transfer_machine.js');
 
-function forAsyncEachLine(stream, line_callback, eof_callback) {
-  var leftovers = '';
-
-  stream.on('data', function(data) {
-    var prev = 0;
-    for (var i = 0, il = data.length; i < il; ++i) {
-      if (data[i] === '\n') {
-        if (i === prev && leftovers.length === 0) {
-          ++prev;
-        } else {
-          line_callback(leftovers + data.substr(prev, i-prev));
-          leftovers = '';
-          prev = i + 1;
-        }
-      }
-    }
-    if (prev !== i) leftovers = data.substr(prev, i-prev);
-  });
-
-  stream.on('close', function() {
-    if (leftovers.length !== 0) {
-      // console.log('No trailing newline...');
-      line_callback(leftovers);
-    }
-    eof_callback();
-  });
+if (process.argv.length > 2) {
+  eval(fs.readFileSync(process.argv[2], 'utf8'));
+} else {
+  console.log("usage: <filename.js>");
+  process.exit(1);
 }
 
 function octets_to_hex_string(octets) {
@@ -36,44 +16,39 @@ function octets_to_hex_string(octets) {
   }).join(' ');
 }
 
-function process_file(filename) {
-  var fs = require('fs');
-  var f = fs.createReadStream(filename, {encoding: 'utf8'});
-  var machine = new transfer_machine.TransferMachine();
-
-  machine.OnControlTransfer = function(addr, endp, setup, data) {
-    console.log("Control transfer: addr: " + addr + " endpoint: " + endp);
-    var bRequest = setup.get_value("bRequest");
-    console.log(setup.debug_string("  "));
-    if (data.length > 0) console.log('    Data: ' + octets_to_hex_string(data));
-    return;
-
-    switch (bRequest) {
-      case 6: // GET_DESCRIPTOR
-        var wvalue = setup.get_value("wValue");
-        var desctype = wvalue >> 8, descidx = wvalue & 0xff;
-        console.log(desctype);
-        console.log(structs.eDescriptorTypes[desctype]);
-        console.log(data);
-        break;
-      default:
-        console.log("Unknown bRequest: " + bRequest);
-        break;
-    }
-  };
-
-  forAsyncEachLine(f, function(line) {
-    var ind = line.indexOf('data=');
-    var packet = [ ];
-    for (i = ind + 5, il = line.length; i < il; i += 3) {
-      packet.push(parseInt(line.substr(i, 3), 16));
-    }
-    if (packet.length !== 0) machine.process_packet(packet);
-  }, function() { });
+function flatten(arr) {
+  return arr.length === 0 ? arr : arr.reduce(function(a, b) { return a.concat(b); });
 }
 
-if (process.argv.length > 2) {
-  process_file(process.argv[2]);
-} else {
-  console.log("usage: <filename>");
+var machine = new transfer_machine.TransferMachine();
+
+machine.OnControlTransfer = function(addr, endp, setup, data) {
+  console.log("Control transfer: addr: " + addr + " endpoint: " + endp);
+  var bRequest = setup.get_value("bRequest");
+  console.log(setup.debug_string("  "));
+  data = flatten(data);
+  if (data.length > 0) console.log('    Data: ' + octets_to_hex_string(data));
+
+  switch (bRequest) {
+    case 6: // GET_DESCRIPTOR
+      var wvalue = setup.get_value("wValue");
+      var desctype = wvalue >> 8, descidx = wvalue & 0xff;
+      console.log(structs.eDescriptorTypes[desctype]);
+      var descriptor = new structs.Fields();
+      console.log(data.length);
+      if (structs.parse_StandardConfigurationDescriptor(descriptor, data, 0, data.length))
+        console.log(descriptor.debug_string("    "));
+      break;
+    default:
+      console.log("Unknown bRequest: " + bRequest);
+      break;
+  }
+};
+
+var decoder = require('./packet_decoder.js');
+for (var i = 0, il = packets.length; i < il; ++i) {
+  var packet = packets[i];
+  console.log(decoder.decode_packet_to_display_string(packet.d));
+  if (packet.d.length !== 0)
+    machine.process_packet(packet.d);
 }
