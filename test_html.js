d525a6d25a87c2a7315f2857504c7143a5941044
Dean McNamee <dean@gmail.com>
2014-11-12 19:52:06 +0100

    Clean up some old state-machine code.

--- test_html.js
+++ test_html.js
@@ -1,6 +1,4 @@
 var fs = require('fs');
-var structs = require('./structs.js');
-var transfer_machine = require('./transfer_machine.js');
 
 function roll_up_js(filename) {
   var src = fs.readFileSync(__dirname + '/' + filename, 'utf8');
@@ -16,43 +14,6 @@ if (process.argv.length > 2) {
   process.exit(1);
 }
 
-function octets_to_hex_string(octets) {
-  return octets.map(function(x) {
-    var str = x.toString(16);
-    return str.length === 1 ? "0" + str : str;
-  }).join(' ');
-}
-
-function flatten(arr) {
-  return arr.length === 0 ? arr : arr.reduce(function(a, b) { return a.concat(b); });
-}
-
-var machine = new transfer_machine.TransferMachine();
-
-machine.OnControlTransfer = function(addr, endp, setup, data) {
-  console.log("Control transfer: addr: " + addr + " endpoint: " + endp);
-  var bRequest = setup.get_value("bRequest");
-  console.log(setup.debug_string("  "));
-  data = flatten(data);
-  if (data.length > 0) console.log('    Data: ' + octets_to_hex_string(data));
-
-  switch (bRequest) {
-    case 6: // GET_DESCRIPTOR
-      var wvalue = setup.get_value("wValue");
-      var desctype = wvalue >> 8, descidx = wvalue & 0xff;
-      console.log(structs.eDescriptorTypes[desctype]);
-      var descriptor = new structs.Fields();
-      console.log(data.length);
-      if (structs.parse_StandardConfigurationDescriptor(descriptor, data, 0, data.length))
-        console.log(descriptor.debug_string("    "));
-      break;
-    default:
-      console.log("Unknown bRequest: " + bRequest);
-      break;
-  }
-};
-
-var decoder = require('./packet_decoder.js');
 console.log('<html><head><style>' + fs.readFileSync(__dirname + '/usbbarev0.css', 'utf8'));
 console.log('</style><script>');
 console.log(fs.readFileSync(process.argv[2], 'utf8'));
