af03b180550c9eb1effe9a4bd4859c9f4f3a660c
Dean McNamee <dean@gmail.com>
2014-11-17 21:04:38 +0100

    Complicated rollup business to save 50k on the html.

--- test_html.js
+++ test_html.js
@@ -1,10 +1,27 @@
 var fs = require('fs');
 
-function roll_up_js(filename) {
+function cleanify_varname(x) {
+  return x.replace(/[^0-9a-zA-Z_]/g, '_');
+}
+
+var rollups = [ ];
+
+function roll_up_js(filename, amroot) {
   var src = fs.readFileSync(__dirname + '/' + filename, 'utf8');
-  return src.replace(/require\('(.*?)'\);/g, function(match, p1) {
-    return '(function(exports) {var module={exports:exports};' + roll_up_js(p1) + ' return module.exports;})({});';
+  src = src.replace(/require\('(.*?)'\)/g, function(match, p1) {
+    var varname = 'import_' + cleanify_varname(p1);
+    rollups.push(varname, '(function(exports) {var module={exports:exports};' +
+                           roll_up_js(p1, false) + ' return module.exports;})({});');
+    return 'import_' + cleanify_varname(p1);
   });
+
+  var rollsrc = '';
+  for (var i = 0, il = rollups.length; amroot && i < il; i += 2) {
+    if (rollups.indexOf(rollups[i]) < i) continue;
+    rollsrc += "var " + rollups[i] + " = " + rollups[i+1] + ";\n";
+  }
+
+  return rollsrc + src;
 }
 
 console.log('<html><head><style>' + fs.readFileSync(__dirname + '/usbbarev0.css', 'utf8'));
@@ -14,5 +31,5 @@ if (process.argv > 2) {
 } else {
   console.log("rawpcapdata = null;");
 }
-console.log('</script><script>' + roll_up_js('/usbbarev0.js'));
+console.log('</script><script>' + roll_up_js('/usbbarev0.js', true));
 console.log('</script><body></body></html>');
